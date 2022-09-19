16172d24bbf76d213c9e47b13e7bff97b5adc139
Kudrat <kudratsultanbaev06@gmail.com>
2022-09-19 21:07:40 +0300

    selected the main html, added the about page

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home_page')
+    path('', views.HomePageView.as_view(), name='home_page'),
+    path('about/', views.AboutPageView.as_view(), name='about_page')
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name ='index.html'
+    template_name ='base.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about/index.html'
